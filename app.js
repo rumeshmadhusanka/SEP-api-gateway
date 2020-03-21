fb3ad34ad88a1c0052c644edb61aba3caf194335
rumesh <rumesh.m.s16@gmail.com>
2020-03-21 18:02:27 +0530

    proxy tested for working

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const ip_filter = require('express-ipfilter').IpFilter;
 const ip_list = require('./IPList');
 const indexRouter = require('./routes/index');
 const sendToMongo = require('./sendToMongo');
-
+const httpProxy = require('express-http-proxy');
+const url = require('url');
 
 const app = express();
 app.set('trust proxy', true);
@@ -13,5 +14,14 @@ app.use(sendToMongo);
 
 app.use('/', indexRouter);
 
+
+const pyProxy = httpProxy(process.env.PROXY1,{
+    preserveHostHdr: true,
+    parseReqBody: false,
+    proxyReqPathResolver: req => url.parse(req.baseUrl).path,
+});
+app.get('/py',async (req,res,next)=>{
+    pyProxy(req,res,next)
+});
 
 module.exports = app;
