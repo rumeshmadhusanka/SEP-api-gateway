e403164583b8e762e13d7af729d5973d211fd477
rumesh <rumesh.m.s16@gmail.com>
2020-04-17 14:18:00 +0530

    js proxy removed

--- app.js
+++ app.js
@@ -40,25 +40,25 @@ const optionsPy = {
 
     },
 };
-const optionsJs = {
-    target: process.env.PROXY2, // target host
-    changeOrigin: true, // needed for virtual hosted sites
-    ws: true, // proxy websockets
-    pathRewrite: {
-        '^/js': '/', // rewrite path
-        // '^/api/remove/path': '/path' // remove base path
-    },
-    router: {
-        // when request.headers.host == 'dev.localhost:3000',
-        // override target 'http://www.example.org' to 'http://localhost:8000'
-
-    },
-};
+// const optionsJs = {
+//     target: process.env.PROXY2, // target host
+//     changeOrigin: true, // needed for virtual hosted sites
+//     ws: true, // proxy websockets
+//     pathRewrite: {
+//         '^/js': '/', // rewrite path
+//         // '^/api/remove/path': '/path' // remove base path
+//     },
+//     router: {
+//         // when request.headers.host == 'dev.localhost:3000',
+//         // override target 'http://www.example.org' to 'http://localhost:8000'
+//
+//     },
+// };
 
 
 const pyProxy = createProxyMiddleware(optionsPy);
-const jsProxy = createProxyMiddleware(optionsJs);
+// const jsProxy = createProxyMiddleware(optionsJs);
 app.use('/py/',pyProxy);
-app.use('/js/',jsProxy);
+// app.use('/js/',jsProxy);
 
 module.exports = app;
