73de71e031cdf566df2aa05697a015a5b6766853
rumesh <rumesh.m.s16@gmail.com>
2020-04-18 21:29:14 +0530

    auth and cache

---
+++ middleware/auth.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+const logout_token_cache = require('cache');
+
+
+const config = {
+    "secret": process.env.JWT_SECRET,
+};
+
+module.exports = {
+    verifyToken: async (req, res, next) => {
+        try {
+            let token = req.headers['x-access-token'];
+            if (!token) {
+                return res.status(403).json({"message": "ERR::LOGIN_TO_PROCEED"});
+            }
+            if (logout_token_cache.get(token)) {
+                return res.status(403).json({"message": "ERR::TOKEN_LOGGED_OUT"});
+            }
+
+            jwt.verify(token, config.secret, (error, decoded) => {
+                if (error) {
+                    return res.status(403).json({"message": "ERR::TOKEN_CORRUPTED"});
+                } else {
+                    next()
+                }
+            })
+        } catch (e) {
+            console.log(e);
+            next()
+        }
+    },
+
+    invalidateToken: async (token) => {
+
+    }
+
+
+};
---
+++ middleware/cache.js
@@ -0,0 +1,68 @@
+const NodeCache = require("node-cache");
+const redis_client = require('./redis');
+const node_cache = new NodeCache({stdTTL: process.env.JWT_TTL, checkperiod: process.env.NODE_CACHE_CHECK_PERIOD});
+
+// cache.set("hello1",true);
+// let val = cache.get("hello");
+// console.log(val);
+
+
+function CacheManager() {
+
+}
+
+CacheManager.prototype.setValue = (value, ttl = process.env.JWT_TTL) => {
+	try {
+		node_cache.set(value, 1, ttl);
+		redis_client.set(value, 1, 'EX', ttl.toString())
+
+	} catch (e) {
+		console.log(e)
+	}
+};
+
+CacheManager.prototype.getValue = (key) => {
+	try {
+		return node_cache.get(key);
+	} catch (e) {
+		console.log(e)
+	}
+};
+
+CacheManager.prototype.syncCacheWithRedis = () => {
+	redis_client.keys('*', function (err, keys) {
+		if (err) return console.log(err);
+
+		for (let i = 0, len = keys.length; i < len; i++) {
+
+			redis_client.get(keys[i], (err, result) => {
+				if (err) {
+					console.log(err)
+				} else {
+					redis_client.ttl(keys[i], (e, t) => {
+						if (e) {
+							console.log(e)
+						} else {
+							console.log(keys[i], result, t);
+							if (t !== -1 || t !== false) {
+								node_cache.set(keys[i], 1, t)
+
+							}
+						}
+					});
+
+				}
+			});
+
+		}
+	});
+
+
+};
+
+const instance = new CacheManager();
+instance.syncCacheWithRedis();
+setInterval(instance.syncCacheWithRedis, process.env.REDIS_NODE_CACHE_SYNC_PERIOD);
+module.exports = instance;
+
+
