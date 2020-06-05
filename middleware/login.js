2332f60ab0f637fc20b5a29e085a4c825fe07e86
rumesh <rumesh.m.s16@gmail.com>
2020-06-05 23:24:51 +0530

    login route completed

--- middleware/login.js
+++ middleware/login.js
@@ -1,31 +1,33 @@
-const express = require('express');
-const router = express.Router();
-const jwt = require('jsonwebtoken');
 const sqlite3 = require('sqlite3').verbose();
 const file = "sql-db";
 const bcrypt = require('bcryptjs');
-const config = {
-	"secret": process.env.JWT_SECRET,
-};
 
-async function login(username, password) {
-	let db = new sqlite3.Database(file);
-	db.all("select username,password from data", async function (err, data) {
-		let db_username = data[0].username;
-		let db_password = data[0].password;
-		console.log(data);
-		console.log(db_password);
-		let result1 = await bcrypt.compare(password, db_password);
-		if (result1 && db_username === username) {
-			db.close();
-			console.log("Match");
-			return true;
-		}else {
-			db.close();
-			console.log("Not a Match");
-			return false;
-		}
-	});
+class Login {
+	async login(username, password) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				let db = new sqlite3.Database(file);
+				await db.all("select username,password from data", (err, data) => {
+					let db_username = data[0].username;
+					let db_password = data[0].password;
+					console.log(data);
+					console.log(db_password);
+					let result1 =  bcrypt.compare(password, db_password);
+					if (result1 && db_username === username) {
+						db.close();
+						console.log("Match");
+						resolve(true);
+					} else {
+						db.close();
+						console.log("Not a Match");
+						resolve(false);
+					}
+				});
+			} catch (e) {
+				reject(e)
+			}
+		});
+	}
 }
 
-module.exports = login;
+module.exports = Login;
--- routes/login.js
+++ routes/login.js
@@ -1,35 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const loginDB = require('../middleware/login');
+const Login = require('../middleware/login');
 const config = {
-    "secret": process.env.JWT_SECRET,
+	"secret": process.env.JWT_SECRET,
 };
-
+let login = new Login();
 
 router.use('/', async (req, res, next) => {
-    let data = "";
-    req.on('data', function(chunk){ data += chunk});
-    req.on('end', function(){
-        req.rawBody = data;
-        req.jsonBody = JSON.parse(data);
-        next();
-    });
+	let data = "";
+	req.on('data', function (chunk) {
+		data += chunk
+	});
+	req.on('end', function () {
+		req.rawBody = data;
+		req.jsonBody = JSON.parse(data);
+		next();
+	});
 });
 
-router.post('/',async (req,res)=>{
-    let username = req.jsonBody.username;
-    let password = req.jsonBody.password;
-    console.log(username);
-    console.log(password);
-    let r = await loginDB(username, password);
-    console.log(r)
-    if (r){
-        let token = jwt.sign({"username": username}, config.secret);
-        res.header('x-access-token', token).status(200).json({"msg":"success"});
-    }else{
-        res.status(403).json({"msg":"failed"});
-    }
+router.post('/', async (req, res) => {
+	let username = req.jsonBody.username;
+	let password = req.jsonBody.password;
+	console.log(username);
+	console.log(password);
+	let r = await login.login(username, password);
+	console.log(r);
+	if (r === true) {
+		let token = jwt.sign({"username": username}, config.secret);
+		res.header('x-access-token', token).status(200).json({"msg": "success"});
+	} else {
+		res.status(403).json({"msg": "failed"});
+	}
 });
 
 
