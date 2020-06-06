c075b1ff07e140c8303e01ce4feb3fe9b0f2a86a
rumesh <rumesh.m.s16@gmail.com>
2020-06-06 12:01:11 +0530

    parsed body for used for routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,17 @@
 module.exports = function (app) {
+	app.use('/', async (req, res, next) => {
+		let data = "";
+		req.on('data', function (chunk) {
+			data += chunk
+		});
+		req.on('end', function () {
+			req.rawBody = data;
+			if (data){
+				req.body = JSON.parse(data);
+			}
+			next();
+		});
+	});
 	app.use("/iplist", require("./iplist"));
 	app.use("/login", require("./login"));
 	//Root route-REMOVE this
