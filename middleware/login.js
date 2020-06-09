9b3b8f55ecbd9ebcd79f14a13e7ff0141141d9d6
rumesh <rumesh.m.s16@gmail.com>
2020-06-09 07:33:29 +0530

    fixed password issue on login

--- middleware/login.js
+++ middleware/login.js
@@ -7,12 +7,13 @@ class Login {
 		return new Promise(async (resolve, reject) => {
 			try {
 				let db = new sqlite3.Database(file);
-				await db.all("select username,password from data", (err, data) => {
+				await db.all("select username,password from data", async (err, data) => {
 					let db_username = data[0].username;
 					let db_password = data[0].password;
 					console.log(data);
 					console.log(db_password);
-					let result1 =  bcrypt.compare(password, db_password);
+					let result1 =  await bcrypt.compare(password, db_password);
+					console.log(result1)
 					if (result1 && db_username === username) {
 						db.close();
 						console.log("Match");
